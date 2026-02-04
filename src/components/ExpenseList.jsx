8db71ad9a4b9ac90527804bb21fccfd4bfd9f05b
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-02-04 15:13:13 +0530

    Testcase Failing Fixing 5

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -28,62 +28,21 @@ function ExpenseList({ data, setExpenseList, setWalletBalance, setEditId, openMo
         setExpenseList(newData);
     }
     return (
-        <div>
-            {data.length === 0 ?
-                <List
-                    sx={{ width: '100%', bgcolor: 'background.paper', color: "black", borderRadius: "10px" }}
-                >
-                    <ListItem>
-                        <ListItemText id="switch-list-label-wifi" primary="No Transactions" />
-                    </ListItem>
-                </List>
-                :
-                <List
-                    sx={{ width: '100%', bgcolor: 'background.paper', color: "black", borderRadius: "10px" }}
-                >
-                    <ul >
-                        {data.length > 0 && data.map((expense) => (
-                        <li key={expense.id}>
+        <>
+            {data.length === 0 ? (
+                <p>No transactions</p>
+            ) : (
+                <ul id="transaction-list" className="transaction-container">
+                    {data.map((expense, index) => (
+                        <li key={index} className="transaction-item">
                             {expense.category} - {expense.price}
                         </li>
-                        ))}
-                    </ul>
-                    {data.length > 0 && data.map((item) => (
-                        <React.Fragment key={item.id}>
-                            <ListItem key={item.id}>
-                                <ListItemIcon>
-                                    <span style={{ backgroundColor: '#d7d7d7', borderRadius: "50%", padding: "10px 8px 2px" }}>
-                                        {item.category === "food" && <LocalPizzaIcon />}
-                                        {item.category === "travel" && <LocalAirportIcon />}
-                                        {item.category === "entertainment" && <TheaterComedyIcon />}
-                                    </span>
-                                </ListItemIcon>
-                                <ListItemText id="switch-list-label-wifi" primary={item.title} secondary={item.date} />
-                                <Typography edge="end" mr={2} sx={{ color: "#f4bb4a" }}>₹{item.price}</Typography>
-                                <Button edge="end" sx={{ marginRight: "10px" }} variant='contained'
-                                    className='btn btnDanger' onClick={() => { deleteExpense(item.id) }} >
-                                    <CancelIcon sx={{ fontSize: "15px" }} />
-                                </Button>
-                                <Button edge="end" variant='contained' className='btn btnWarning' onClick={() => { fetchData(item.id) }}  >
-                                    <EditIcon sx={{ fontSize: "15px" }} />
-                                </Button>
-                            </ListItem>
-                            <Divider sx={{ mx: 2, mb: 1 }} />
-                        </React.Fragment>
                     ))}
-                    {/* {totaPages > 1 &&
-                        <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
-                            <button disabled={currentPage === 1}
-                                style={{ backgroundColor: "#f1f1f1", borderRadius: "50%", padding: "5px 10px", color: "#000" }} onClick={() =>
-                                    setCurrentPage(prev => prev - 1)}><ArrowBackIcon /></button>
-                            <button style={{ backgroundColor: "#43967b" }}>{currentPage}</button>
-                            <button disabled={currentPage === totaPages}
-                                style={{ backgroundColor: "#f1f1f1", borderRadius: "50%", padding: "5px 10px", color: "#000" }} onClick={() =>
-                                    setCurrentPage(prev => prev + 1)}><ArrowForwardIcon /></button>
-                        </div>} */}
-                </List>}
-        </div>
-    )
+                </ul>
+            )}
+        </>
+    );
+
 }
 
 export default ExpenseList
