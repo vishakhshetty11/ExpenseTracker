7c8da672d6b2c798c84557b7993632db0914ab87
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-02-11 16:30:12 +0530

    Final Commit

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -14,6 +14,11 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import "./ExpenseList.css"
 function ExpenseList({ data, setExpenseList, setWalletBalance, setEditId, openModal }) {
 
+    const maxRecord = 3;
+    const [currentExpenseList, setCurrentExpenseList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totaPages, setTotalPages] = useState(0);
+
     const fetchData = (id) => {
         setEditId(id);
         openModal(true);
@@ -25,6 +30,18 @@ function ExpenseList({ data, setExpenseList, setWalletBalance, setEditId, openMo
         const newData = data.filter(item => item.id != id);
         setExpenseList(newData);
     }
+    useEffect(() => {
+        const startIndex = (currentPage - 1) * maxRecord;
+        const lastIndex = Math.min(currentPage * maxRecord, data.length);
+        const currentList = data.slice(startIndex, lastIndex);
+        setCurrentExpenseList(currentList);
+        setTotalPages(Math.ceil(data.length / maxRecord))
+    }, [currentPage, data]);
+    useEffect(() => {
+        if (totaPages < currentPage && currentPage > 1) {
+            setCurrentPage(prev => Number(prev) - 1)
+        }
+    }, [totaPages])
     return (
         <div>
             {data.length === 0 ?
@@ -36,13 +53,43 @@ function ExpenseList({ data, setExpenseList, setWalletBalance, setEditId, openMo
                     </ListItem>
                 </List>
                 :
-                <ul id="transaction-list" className="transaction-container">
-                    {data.map((expense, index) => (
-                        <li key={index} className="transaction-item">
-                            {expense.category.charAt(0).toUpperCase() + expense.category.slice(1)} - {expense.price}
-                        </li>
+                <List
+                    sx={{ width: '100%', bgcolor: 'background.paper', color: "black", borderRadius: "10px" }}
+                >
+                    {currentExpenseList.length > 0 && currentExpenseList.map((item) => (
+                        <React.Fragment key={item.id}>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <span style={{ backgroundColor: '#d7d7d7', borderRadius: "50%", padding: "10px 8px 2px" }}>
+                                        {item.category === "food" && <LocalPizzaIcon />}
+                                        {item.category === "travel" && <LocalAirportIcon />}
+                                        {item.category === "entertainment" && <TheaterComedyIcon />}
+                                    </span>
+                                </ListItemIcon>
+                                <ListItemText id="switch-list-label-wifi" primary={item.title} secondary={item.date} />
+                                <Typography edge="end" mr={2} sx={{ color: "#f4bb4a" }}>₹{item.price}</Typography>
+                                <Button edge="end" sx={{ marginRight: "10px" }} variant='contained'
+                                    className='btn btnDanger' onClick={() => { deleteExpense(item.id) }} >
+                                    <CancelIcon sx={{ fontSize: "15px" }} />
+                                </Button>
+                                <Button edge="end" variant='contained' className='btn btnWarning' onClick={() => { fetchData(item.id) }}  >
+                                    <EditIcon sx={{ fontSize: "15px" }} />
+                                </Button>
+                            </ListItem>
+                            <Divider sx={{ mx: 2, mb: 1 }} />
+                        </React.Fragment>
                     ))}
-                </ul>}
+                    {totaPages > 1 &&
+                        <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+                            <button disabled={currentPage === 1}
+                                style={{ backgroundColor: "#f1f1f1", borderRadius: "50%", padding: "5px 10px", color: "#000" }} onClick={() =>
+                                    setCurrentPage(prev => prev - 1)}><ArrowBackIcon /></button>
+                            <button style={{ backgroundColor: "#43967b" }}>{currentPage}</button>
+                            <button disabled={currentPage === totaPages}
+                                style={{ backgroundColor: "#f1f1f1", borderRadius: "50%", padding: "5px 10px", color: "#000" }} onClick={() =>
+                                    setCurrentPage(prev => prev + 1)}><ArrowForwardIcon /></button>
+                        </div>}
+                </List>}
         </div>
     )
 }
